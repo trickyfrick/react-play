654e5c5327047ed6f2c3e1c9575cc409375a3c99
David Frick <davidfrick@Davids-iMac.local>
2017-11-10 12:01:54 -0800

    Basic project with webpack and react

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import App from './containers/App'
+
+ReactDOM.render(<App/>, document.getElementById('app'));
